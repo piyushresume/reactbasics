c7a4d80d56bf3be8add04666cbff7aa680c86d82
Piyush <piy.sky7@gmail.com>
2020-09-15 23:00:31 +0530

    Updates

---
+++ precache-manifest.4396428fc87d21de00477a3084a18d16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306a7c47f926f4e401849d70529cace5",
+    "url": "/reactbasics/index.html"
+  },
+  {
+    "revision": "6c6f4206d69bda942aa7",
+    "url": "/reactbasics/static/css/main.9975199d.chunk.css"
+  },
+  {
+    "revision": "946392950e3e6f5c0541",
+    "url": "/reactbasics/static/js/2.92e4ea7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactbasics/static/js/2.92e4ea7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6f4206d69bda942aa7",
+    "url": "/reactbasics/static/js/main.09e24ff5.chunk.js"
+  },
+  {
+    "revision": "fb83620558b82384096d",
+    "url": "/reactbasics/static/js/runtime-main.19308c31.js"
+  }
+]);
